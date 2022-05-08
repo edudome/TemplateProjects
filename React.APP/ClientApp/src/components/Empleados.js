7eee474989791e0ceb7ca0319593e1e803b10894
Eduardo Domenech <edudome9@outlook.com>
2022-05-08 13:27:59 -0300

    react app

---
+++ React.APP/ClientApp/src/components/Empleados.js
@@ -0,0 +1,24 @@
+﻿import React, { Component, useState } from 'react';
+
+export function EmpleadosList({empleados}) {
+
+    return (
+        <ul>
+            {empleados.map((empleado) => (<li></li>))}
+        </ul>
+    )
+}
+
+export function ListarEmpleados() {
+
+    const [empleados, setEmpleados] = useState([
+        { id: 1, Nombre: "Eduardo", Habilitado: true }
+    ]);
+
+    return (
+        <div>
+            <div>Lista de Empleados:</div>
+            <EmpleadosList empleados={empleados} />
+        </div>
+    );
+}
---
+++ React.APP/ClientApp/src/components/Greeting.js
@@ -0,0 +1,33 @@
+﻿import React, { Component } from 'react';
+
+export class Greeting extends Component {
+    static displayName = Greeting.name;
+
+    constructor(props) {
+        super(props);
+        this.state = { name: '' };
+    }
+
+    handleChange = event => {
+        this.setState({ name: event.target.value });
+    };
+
+    render() {
+        return (
+            <React.Fragment>
+                <form>
+                    <label htmlFor="name">Name</label>
+                    <span>&nbsp;&nbsp;</span>
+                    <input
+                        type="text"
+                        name="name"
+                        value={this.state.name}
+                        onChange={this.handleChange}
+                    />
+                </form>
+
+                <h3>Hello {this.state.name}, Good morning!</h3>
+            </React.Fragment>
+        );
+    }
+}
